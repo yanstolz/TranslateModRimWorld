a182bfb75636bddbc599a5abbef9cc5c7838924c
Ivan Bocharov <sir.ivan-akotya@yandex.ru>
2022-11-29 13:46:02 +0300

    The first version of the program. Manual selection of the XML files to be translated is required.

---
+++ TranslateMod/Translate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Xml;
+using System.Net;
+using System.Web;
+
+namespace TranslateMod
+{
+    public class Translate
+    {
+
+        public void StartTranslateXML(string path)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(path);
+            XmlElement element = xml.DocumentElement;
+            foreach (XmlNode xnode in element)
+            {
+                xnode.InnerText = TranslateWord(xnode.InnerText);
+            }
+            xml.Save(path);
+        }
+
+        public string TranslateWord(string word)
+        {
+            var toLanguage = "ru";//Русский
+            var fromLanguage = "en";//Английский
+            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={HttpUtility.UrlEncode(word)}";
+            var webClient = new WebClient
+            {
+                Encoding = System.Text.Encoding.UTF8
+            };
+            var result = webClient.DownloadString(url);
+            try
+            {
+                result = result.Substring(4, result.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
+                return result;
+            }
+            catch
+            {
+                return "Ошибка";
+            }
+        }
+    }
+}
---
+++ TranslateModRimWorld/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Win32;
+using System.IO;
+using TranslateMod;
+
+namespace TranslateModRimWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Translate translate = new Translate();
+        public MainWindow()
+        {
+            InitializeComponent();
+            MessageBox.Show("Данная программа использует обычный переводчик, возможен кривой перевод");
+        }
+
+        private void bStartTranslate_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (object item in lbFiles.Items)
+            {
+                translate.StartTranslateXML(Convert.ToString(item));
+            }
+            MessageBox.Show("Перевод выполнен, удачной игры");
+        }
+
+        private void addFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Xml files (*.xml)|*.xml|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                    lbFiles.Items.Add(filename);
+            }
+        }
+    }
+}
